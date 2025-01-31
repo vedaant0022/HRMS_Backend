862ab827686d4ff8e0f3fa72799cfff097235961
Vedaant Sankhe <vedaant.sankhe@gmail.com>
2025-01-31 11:54:46 +0530

    server fix-2

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/users', userRoutes);
 
 app.get('/',(req,res)=>{
-    res.end("Welcome to HRMS Management System")
+    res.end("Welcome to HRMS Management System || Server is Live|| Port == "+port)
 })
 
 
 app.listen(port,()=>{
     console.log("Working ", port) 
     console.log(`API Docs available at http://localhost:${port}/api-docs`);
+    console.log(`Server Live at http://localhost:${port}/`);
 })
 
